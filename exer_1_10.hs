3e8a1ac700ed2d5a5eea8e6f990cc67637def3a8
Rui Peres <sauron.is.alive@gmail.com>
2014-11-28 00:30:21 +0000

    Added exercise 2

--- exer_1_10.hs
+++ exer_1_10.hs
@@ -3,5 +3,7 @@ exerc1 (x:[]) = x
 exerc1 (_:xs) = exerc1 xs
 
 exerc2 :: [a] -> a
-exerc2 (x:[]) = x
-exerc2 (_:xs) = exerc1 xs
+exerc2 [] = error "No end for empty lists!"
+exerc2 (x:[]) = error "No end for empty lists!"
+exerc2 (x:xs:[]) = x
+exerc2 (x:xs) = exerc2 xs
