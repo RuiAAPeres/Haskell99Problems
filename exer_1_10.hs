695380e5db3efac4c3649c2ee537d1f6cbf8d4d6
Rui Peres <sauron.is.alive@gmail.com>
2014-12-14 23:51:57 +0000

    Added problem 9

--- exer_1_10.hs
+++ exer_1_10.hs
@@ -10,3 +10,8 @@ exerc2 (x:xs) = exerc2 xs
 
 exerc2' :: [a] -> a
 exerc2' = (head . tail . reverse)
+
+
+exerc9 :: (Eq a) => [a] -> [[a]]
+exerc9 [] = []
+exerc9 r@(x:xs) = (takeWhile (==x) r : exerc9 (dropWhile (==x) xs))
